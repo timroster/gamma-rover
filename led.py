d5a2845280cb6f2d74f5cde0ef440bfdb8f0cdff
timroster <timroster@gmail.com>
2015-04-11 20:51:00 -0700

    Create led.py
    Python script to turn on one of three colored leds connected to raspberry pi expansion header. Red connected to pin 22, yellow to pin 18 and green to 16 all with a 660 ohm resistor to pin 6 (ground).

---
+++ led.py
@@ -0,0 +1,30 @@
+import time
+import RPi.GPIO as GPIO
+import sys 
+
+# the red led cathode is wired to pin 22 of the expansion header
+# the yellow led cathode is wired to pin 18 of the expansion header
+# the green led cathode is wired to pin 16 of the expansion header
+REDLED = 22
+YELLOWLED = 18
+GREENLED = 16
+
+# set up control for the channel and pull to a known state
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(REDLED, GPIO.OUT)
+GPIO.output(REDLED, GPIO.LOW)
+GPIO.setup(YELLOWLED, GPIO.OUT)
+GPIO.output(YELLOWLED, GPIO.LOW)
+GPIO.setup(GREENLED, GPIO.OUT)
+GPIO.output(GREENLED, GPIO.LOW)
+
+# expect to be invoked as color.py {red|yellow|green}
+# with no arguments defaults to turning off all led
+if len(sys.argv) == 2:
+        if sys.argv[1] == "red":
+		GPIO.output(REDLED, GPIO.HIGH)
+	if sys.argv[1] == "yellow":	
+		GPIO.output(YELLOWLED, GPIO.HIGH)
+	if sys.argv[1] == "green":	
+		GPIO.output(GREENLED, GPIO.HIGH)
